f44f26e9302b6f06dfd60851552e8316aa0d0ffe
dodoma26 <makssobol@mail.ru>
2016-03-24 18:40:03 +0400

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
-define("MYSQL_SERVER",'localhost');
-define("MYSQL_USER",'root');
-define("MYSQL_PASSWORD",'');
-define("MYSQL_DB",'sity');
+define("MYSQL_SERVER",'ap-cdbr-azure-east-c.cloudapp.net');
+define("MYSQL_USER",'b43dc0b08563a6');
+define("MYSQL_PASSWORD",'aa4f3c1a');
+define("MYSQL_DB",'articles');
 
 function db_connect(){
     $link = mysqli_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB) or die("Error: ".mysqli_error($link));
