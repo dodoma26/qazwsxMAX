b14443401c1f78471cdaa903975a71515ec4f271
dodoma26 <makssobol@mail.ru>
2016-03-25 18:29:19 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html>
-<?php
-require_once("database.php");
-require_once("articles.php");
-
-$link = db_connect();
-$articles = articles_all($link);
-include("main.php");
-?>
-</html>
+<script>
+  
+  alert( "Привет" );
+</script>
