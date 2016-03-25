153c8fc1c3562be7a5a82a341389662631d24baa
dodoma26 <makssobol@mail.ru>
2016-03-25 18:30:35 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 
-<!DOCTYPE html>
-<html>
-<?php
-require_once("database.php");
-require_once("articles.php");
-
-$link = db_connect();
-$articles = articles_all($link);
-include("main.php");
-?>
-</html>
+<script>
+  
+  alert( "Привет" );
+</script>
